ea0953f29239241c4fd1f5b4f0d643bc49235bc0
Matthew Kay <matthew.kay@gmail.com>
2014-12-24 14:11:47 -0800

    properly generate monthly and annual income

--- chapter_5_generate.r
+++ chapter_5_generate.r
@@ -2,28 +2,45 @@
 source("generate.R")
 source("chapter_5_functions.R")
 
+#put all monthly data in this table
+monthly = email
+
+
+
+#PART IV
 # add monthly number of sales to the email response time data. This is 
 # inversely proportional to response time, with some added noise
-email$monthlySales = floor(10 * pmax(email$responseTime + rnorm(nrow(email)), .05)^-0.5)
-
+monthly$monthlySales = floor(10 * pmax(monthly$responseTime + rnorm(nrow(monthly)), .05)^-0.5)
 
 # generate salary based on base salary plus some commission from above sales
-averageOrderValue = 500
+averageOrderValue = 5000
 commission = .2
-baseSalary = 30000
-email$annualIncome = baseSalary * email$monthlySales * 12 * averageOrderValue * commission
+baseSalary = 30000 / 12 #base monthly salary
+monthly$monthlyIncome = baseSalary + monthly$monthlySales * averageOrderValue * commission
+
+#annual salary will be the same as the sum of each person's monthly salaries
+#over the 4 month period times 12/4
+annual = ddply(monthly, ~ uid + team + teamName, summarize, annualIncome=sum(monthlyIncome) * 12/4)
 
 
 
+#SAVE DATA TABLES
+write.csv(monthly, "data/chapter_5_monthly_data.csv")
+write.csv(annual, "data/chapter_5_annual_data.csv")
 
-#make some reference plots
-pdf("chapter_5_plots.pdf")
+
+
+#REFERENCE PLOTS
+pdf("plots/chapter_5.pdf")
 tryCatch({
     # monthly sales versus email response time
-    print(ggplot(email, aes(x=responseTime, y=monthlySales, color=teamName)) + geom_point() + geom_hline(yintercept=0)) 
+    print(ggplot(monthly, aes(x=responseTime, y=monthlySales, color=teamName)) + geom_point() + geom_hline(yintercept=0)) 
+    
+    # monthly income of each team
+    print(ggplot(monthly, aes(x=monthlyIncome)) + geom_histogram() + facet_wrap(~teamName))
     
     # annual income of each team
-    print(ggplot(email, aes(x=annualIncome)) + geom_histogram() + facet_wrap(~teamName))
+    print(ggplot(annual, aes(x=annualIncome)) + geom_histogram() + facet_wrap(~teamName))
 }, finally={
     dev.off()  
 })
